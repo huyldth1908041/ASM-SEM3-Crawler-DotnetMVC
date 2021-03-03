e8b94ae6a9278a18bde1f7410bcf55961855a906
GnourtPrond97 <truonghhth1905001@gmail.com>
2021-03-03 19:29:55 +0700

    Add view list resource

--- ReadNewsWebClient/ReadNewsWebClient/Controllers/AdminController.cs
+++ ReadNewsWebClient/ReadNewsWebClient/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReadNewsWebClient.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -19,6 +20,28 @@ namespace ReadNewsWebClient.Controllers
             return View();
         }
 
-       
+        public ActionResult ListResource()
+        {
+
+            var list = new List<CrawlerConfigsViewModel>()
+            {
+             new CrawlerConfigsViewModel(){
+                 Path ="doi-sang",
+                 Route="vnespress"
+             }
+            };
+            return View(list);
+        }
+
+        public void  RunUrlCrawler()
+        {
+
+        }
+
+        public void RunContentCrawler()
+        {
+
+        }
+
     }
 }
