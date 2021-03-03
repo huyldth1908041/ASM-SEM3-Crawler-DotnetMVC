11d9b3a565aabecb16ed531fb24a9f933a3da7d2
GnourtPrond97 <truonghhth1905001@gmail.com>
2021-03-03 19:01:08 +0700

    apply admin template

---
+++ ReadNewsWebClient/ReadNewsWebClient/Models/CrawlerConfigsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReadNewsWebClient.Models
+{
+    public class CrawlerConfigsViewModel
+    {
+        public string Path { get; set; }
+        public string Route { get; set; }
+        public string LinkSelector { get; set; }
+        public string ContentSelector { get; set; }
+        public string TitleSelector { get; set; }
+        public string DescriptionSelector { get; set; }
+        public string RemovalSelector { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
