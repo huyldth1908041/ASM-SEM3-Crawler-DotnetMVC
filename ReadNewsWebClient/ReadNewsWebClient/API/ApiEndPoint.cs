8a74e2dc82977c57afbb20e5609fa33c253589a5
huyldth1904081 <huyldth1904081@fpt.edu.vn>
2021-03-04 10:09:11 +0700

    update master branch

---
+++ ReadNewsWebClient/ReadNewsWebClient/API/ApiEndPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReadNewsWebClient.API
+{
+    public class ApiEndPoint
+    {
+        public static string ApiDomain = "https://localhost:44394";
+        public static string LoginPath = "/oauth/token";
+        public static string GetUserInfoPath = "/api/Account/GetUserInfo";
+        public static string RunUrlCrawlerPath = "/api/Bot/RunUrlCrawlerBot";
+        public static string GetListCrawlerConfigPath = "/api/CrawlerConfig/GetListConfig";
+    }
+}
