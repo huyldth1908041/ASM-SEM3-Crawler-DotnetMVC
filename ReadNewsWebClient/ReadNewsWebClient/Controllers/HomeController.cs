cb52f74d6789a9234a002844292b9a5702b259b7
huyldth1904081 <huyldth1904081@fpt.edu.vn>
2021-03-10 20:13:47 +0700

    fix null reference in view article/index and add authen filter

--- ReadNewsWebClient/ReadNewsWebClient/Controllers/HomeController.cs
+++ ReadNewsWebClient/ReadNewsWebClient/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReadNewsWebClient.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +7,7 @@ using System.Web.Mvc;
 
 namespace ReadNewsWebClient.Controllers
 {
+    [InitSessionFilter]
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -13,14 +15,14 @@ namespace ReadNewsWebClient.Controllers
             
             return View();
         }
-
+        [AdminFilter]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
+        [AuthenticationFilter]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
---
+++ ReadNewsWebClient/ReadNewsWebClient/Models/ViewModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReadNewsWebClient.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+    public class UserInforViewModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string RollNumber { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+    }
+
+    public class ArticleIndexViewModel
+    {
+        public List<Article> TopFivesLatest { get; set; }
+        public List<Category> ListCategories { get; set; }
+        public List<Article> AllArticles { get; set; }
+
+        public List<Article> TrendingAricles { get; set; }
+
+        public ArticleIndexViewModel()
+        {
+            TopFivesLatest = new List<Article>();
+            ListCategories = new List<Category>();
+            AllArticles = new List<Article>();
+            TrendingAricles = new List<Article>();
+        }
+    }
+}
